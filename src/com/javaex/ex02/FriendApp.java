e2de6d48256318a6e379c256ff1730ef5dcf317e
seed-yoo <lukdown10@naver.com>
2024-01-14 21:44:56 +0900

    수정(완)

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,11 +1,48 @@
 package com.javaex.ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
 
-
+    	Scanner sc = new Scanner(System.in);
+    	List<Friend> fList = new ArrayList<Friend>();
+    	
+    	System.out.println("친구를 3명 등록해주세요");
+    	
+    	
+    	String f_info1 = sc.nextLine();
+    	String[] fArray01 = f_info1.split(" ");
+    	String f_info2 = sc.nextLine();
+    	String[] fArray02 = f_info2.split(" ");
+    	String f_info3 = sc.nextLine();
+    	String[] fArray03 = f_info3.split(" ");
+    	
+    	Friend f1 = new Friend(fArray01[0], fArray01[1], fArray01[2]);
+    	Friend f2 = new Friend(fArray02[0], fArray02[1], fArray02[2]);
+    	Friend f3 = new Friend(fArray03[0], fArray03[1], fArray03[2]);
+    	
+    	fList.add(f1);
+    	fList.add(f2);
+    	fList.add(f3);
+    	
+    	for (int i = 0; i < fList.size(); i++) {
+			fList.get(i).showInfo();
+		}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	sc.close();
     	
     	
     }
--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,10 +1,45 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-       
-    }
+		Scanner sc = new Scanner(System.in);
+		boolean tf = true;
+		int totalCount = 0;
+		List<Goods> gList = new ArrayList<Goods>();
+
+		System.out.println("상품을 입력해주세요(종료 q)");
+
+		do {
+			String goods_info;
+			goods_info = sc.nextLine();
+			if (goods_info.equals("q")) {
+				System.out.println("[입력완료]");
+				break;
+			}
+			String[] gArray = goods_info.split(",");
+
+			int gNum = Integer.parseInt(gArray[1]);
+			int gNum2 = Integer.parseInt(gArray[2]);
+
+			Goods gd = new Goods(gArray[0], gNum, gNum2);
+			gList.add(gd);
+
+		} while (tf);
+
+		for (int i = 0; i < gList.size(); i++) {
+			gList.get(i).showInfo();
+			totalCount += gList.get(i).getCount();
+		}
+
+		System.out.println(totalCount);
+
+		sc.close();
+	}
 
 }
